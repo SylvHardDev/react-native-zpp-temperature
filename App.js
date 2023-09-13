40ebf90f01a2a8711ef9478dcf795a3aad1db9d6
Hardy Sylvestre <hardsyldev@gmail.com>
2023-09-13 19:34:27 +0300

    setup du layout

--- App.js
+++ App.js
@@ -1,102 +1,23 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native'; // Assuming you're using React Navigation
-// import { FontAwesome } from '@expo/vector-icons'; 
+import { ImageBackground, Text, View } from "react-native-web";
+import { s } from "./App.style";
+import hotBackground from './assets/2.3_hot.png'
 
-const Header = () => {
-  const navigation = useNavigation();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
+export default function App() {
   return (
-    <View style={styles.header}>
-      <View style={styles.navWrapper}>
-        <TouchableOpacity onPress={toggleMenu} style={styles.mobileMenu}>
-          <Text style={styles.closeIcon}>❌</Text>
-        </TouchableOpacity>
-
-        <View style={styles.navigation}>
-          <View style={styles.logo}>
-            <Text style={styles.logoText}>
-              Visio<Text style={styles.logoSpan}>medico.</Text>
-            </Text>
+    <>
+      <ImageBackground source={hotBackground} style={s.container}>
+        <View style={s.workspace}>
+          <View>
+            <Text>Temperature</Text>
           </View>
-
-          <View style={isMenuOpen ? styles.menuActive : styles.menu}>
-            <TouchableOpacity onPress={toggleMenu} style={styles.mobileMenuClose}>
-              <Text style={styles.closeIcon}>❌</Text>
-            </TouchableOpacity>
-            <View style={styles.navItem}>
-              <TouchableOpacity
-                onPress={() => {
-                  toggleMenu();
-                  navigation.navigate('Home'); // Use your navigation routes
-                }}
-                style={styles.navLink}
-              >
-                <Text style={styles.navText}>Home</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.navItem}>
-              <TouchableOpacity
-                onPress={() => {
-                  toggleMenu();
-                  navigation.navigate('Doctor'); // Use your navigation routes
-                }}
-                style={styles.navLink}
-              >
-                <Text style={styles.navText}>Docteurs</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.navItem}>
-              <TouchableOpacity
-                onPress={() => {
-                  toggleMenu();
-                  navigation.navigate('Service'); // Use your navigation routes
-                }}
-                style={styles.navLink}
-              >
-                <Text style={styles.navText}>Services</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.navItem}>
-              <TouchableOpacity
-                onPress={() => {
-                  toggleMenu();
-                  navigation.navigate('Autre'); // Use your navigation routes
-                }}
-                style={styles.navLink}
-              >
-                <Text style={styles.navText}>Autre</Text>
-              </TouchableOpacity>
-            </View>
+          <View>
+            <Text>input</Text>
+          </View>
+          <View>
+            <Text>button</Text>
           </View>
         </View>
-
-        <View style={styles.loginLogout}>
-          <View style={styles.login}>
-            <TouchableOpacity
-              onPress={() => navigation.navigate('Login')}
-              style={styles.loginButton}
-            >
-              <Text style={styles.loginButtonText}>Signin</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.logout}>
-            <TouchableOpacity
-              onPress={() => navigation.navigate('Register')}
-              style={styles.signupButton}
-            >
-              <Text style={styles.signupButtonText}>Signup</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    </View>
+      </ImageBackground>
+    </>
   );
-};
-
-export default Header;
+}
